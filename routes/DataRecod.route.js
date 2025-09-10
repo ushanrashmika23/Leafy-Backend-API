6f0aa4f8ccdbdc73495228a07e6f146b2e868a6f
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-09-10 08:04:38 +0530

    dataRec path updated

--- routes/DataRecod.route.js
+++ routes/DataRecod.route.js
@@ -1,8 +1,8 @@
-const UserPlantController = require('../controller/UserPlant.controller');
+const DataRecod = require('../controller/DataRecod.controller');
 const express = require('express');
 const router = express.Router();
 
-router.post('/add', UserPlantController.addPlant);
-router.get('/all', UserPlantController.getPlants);
+router.post('/add', DataRecod.addNewRecod);
+router.get('/all', DataRecod.getAllRecods);
 
 module.exports = router;
