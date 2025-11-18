ca5a98876a8e29e77fd80e646f341c87506ecb93
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-11-18 20:47:41 +0530

    added .env file

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = 3004;
-const DBSTRING = 'mongodb+srv://ushanrashmika23_db_user:1vZE8sjB1mW4RF6g@cluster-leafy.54pzqyh.mongodb.net/leafy?retryWrites=true&w=majority&appName=Cluster-leafy';
+const PORT = process.env.PORT || 3004;
+const DBSTRING = process.env.MONGODB_URI || 'mongodb+srv://ushanrashmika23_db_user:1vZE8sjB1mW4RF6g@cluster-leafy.54pzqyh.mongodb.net/leafy?retryWrites=true&w=majority&appName=Cluster-leafy';
 
 app.get('/test', (req, res) => {
     res.send('Leafy Server is working smoothly...');
@@ -19,7 +20,10 @@ app.use('/userplants', UserPlantRoutes);
 const DataRecodRoutes = require('./routes/DataRecod.route');
 app.use('/datarecods', DataRecodRoutes);
 
-mongoose.connect(DBSTRING)
+mongoose.connect(DBSTRING, {
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000, // 45 seconds
+})
     .then(() => console.log('### ## # MongoDB connected'))
     .catch((error) => console.error('!!! !! ! MongoDB connection error:', error));
 
