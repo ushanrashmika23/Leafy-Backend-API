066c48df7f6864396fb0d2a1be05346cac443202
Ushan Rashmika <ushanrashmika23@gmail.com>
2025-11-26 20:23:27 +0530

    changed index structure

--- index.js
+++ index.js
@@ -11,26 +11,26 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 3004;
 const DBSTRING = process.env.MONGODB_URI || 'mongodb+srv://ushanrashmika23_db_user:CYwfIXiWx63MELeE@cluster-leafy.54pzqyh.mongodb.net/leafy?retryWrites=true&w=majority&appName=Cluster-leafy';
 
+// Load routes
+const UserPlantRoutes = require('./routes/UserPlant.route');
+const DataRecodRoutes = require('./routes/DataRecod.route');
+const ControllerDeviceRoutes = require('./routes/ControllerDevice.route');
+
+mongoose.connect(DBSTRING)
+    .then(() => {
+        console.log('MongoDB connected');
+        // Use routes
+        app.use('/userplants', UserPlantRoutes);
+        app.use('/datarecods', DataRecodRoutes);
+        app.use('/controldevices', ControllerDeviceRoutes);
+
+        // Now start server
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => console.error('MongoDB connection error:', error));
+
 app.get('/test', (req, res) => {
     res.send('Leafy Server is working smoothly...');
-});
-
-const UserPlantRoutes = require('./routes/UserPlant.route');
-app.use('/userplants', UserPlantRoutes);
-const DataRecodRoutes = require('./routes/DataRecod.route');
-app.use('/datarecods', DataRecodRoutes);
-const ControllerDeviceRoutes = require('./routes/ControllerDevice.route');
-app.use('/controldevices', ControllerDeviceRoutes);
-
-mongoose.connect(DBSTRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // 30 seconds
-    socketTimeoutMS: 45000, // 45 seconds
-})
-    .then(() => console.log('### ## # MongoDB connected'))
-    .catch((error) => console.error('!!! !! ! MongoDB connection error:', error));
-
-app.listen(PORT, () => {
-    console.log(`### ## # Server is running on port ${PORT}`);
 });
